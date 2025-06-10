2748a446b72d9dbcd4b6232f31ee8cfa022e8d81
Ken Riley <kenr@nodots.com>
2025-06-09 19:28:23 -0600

    Adding testing coverage to README

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: './src',
+  coverageDirectory: '../coverage',
   verbose: true,
   collectCoverage: true,
+  coverageReporters: ['text', 'lcov'],
   testMatch: ['**/?(*.)+(test).ts'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   transform: {
